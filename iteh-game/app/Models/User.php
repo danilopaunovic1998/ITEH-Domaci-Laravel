fcc32212e69d0951ed63d66206565b74e8ad4a2d
danilopaunovic1998 <57719918+danilopaunovic1998@users.noreply.github.com>
2022-08-20 21:38:20 +0200

    Kreiranje user*-*champion migracije i veze

--- iteh-game/app/Models/User.php
+++ iteh-game/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function championsOwned()
+    {
+        return $this->belongsToMany(Champion::class, 'user_champion_relationship');
+    }
 }
---
+++ iteh-game/database/migrations/2022_08_20_192115_create_user_champion_relationship_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_champion_relationship', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('champion_id');
+            $table->unique(['user_id', 'champion_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_champion_relationship');
+    }
+};
