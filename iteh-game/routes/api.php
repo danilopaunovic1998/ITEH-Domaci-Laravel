7701a974564d1205e7ed39e8f3ec10a4b1ac52ad
danilopaunovic1998 <57719918+danilopaunovic1998@users.noreply.github.com>
2022-08-23 23:35:37 +0200

    Podesavanje svih ruta

--- iteh-game/routes/api.php
+++ iteh-game/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ChampionController;
 use App\Http\Controllers\ChampionSkinController;
 use App\Http\Controllers\SkinController;
 use App\Http\Controllers\UserChampionController;
+use App\Http\Controllers\UserSkinController;
 use App\Http\Resources\ChampionResource;
 use App\Http\Resources\SkinResource;
 use Illuminate\Http\Request;
@@ -21,21 +22,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('/champions', ChampionController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+Route::resource('/skins', SkinController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+Route::resource('champions.skins', ChampionSkinController::class)->only('index');
 
 // Route::get('/champions', [ChampionController::class,'index']);
 // Route::get('/champions/{id}', [ChampionController::class,'show']);
 
-Route::resource('/champions',ChampionController::class)->only(['index', 'show', 'store','update', 'destroy']);
-//Route::resource('/skins', SkinController::class)->only(['index', 'show', 'store','update', 'destroy']);
-//Route::resource('champions.skins', ChampionSkinController::class)->only('index');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('profile', function (Request $request) {
+        return auth()->user();
+    });
+    Route::get('/profile/champions', [UserChampionController::class, 'index']);
+    Route::post('/profile/champions/buy/{champion_id}', [UserChampionController::class, 'store']);
+    Route::delete('/profile/champions/sell/{champion_id}', [UserChampionController::class, 'destroy']);
 
-Route::get('/profile/{user_id}', [UserChampionController::class, 'index']);
-Route::post('/profile/{user_id}/buy/{champion_id}', [UserChampionController::class, 'store']);
-Route::delete('/profile/{user_id}/sell/{champion_id}',[UserChampionController::class,'destroy']);
+    Route::get('/profile/skins', [UserSkinController::class, 'index']);
+    Route::post('/profile/skins/buy/{skin_id}', [UserSkinController::class, 'store']);
+    Route::delete('/profile/skins/sell/{skin_id}',[UserSkinController::class,'destroy']);
 
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Route::get('/profile/{user_id}', [UserChampionController::class, 'index']);
